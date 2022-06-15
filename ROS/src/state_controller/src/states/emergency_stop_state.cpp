dcde9e88555afe2788b1e0f1175969dade277b1e
rpvos <rik.vos01@gmail.com>
2022-06-15 13:54:42 +0200

    [Fix] Merge conflict

---
+++ ROS/src/state_controller/src/states/emergency_stop_state.cpp
@@ -0,0 +1,9 @@
+#include "state_controller/robot_state.h"
+
+void emergency_stop_state::update(){
+
+}
+
+void emergency_stop_state::on_cmd_vel_recieve(const geometry_msgs::Twist::ConstPtr& msg){
+
+}
